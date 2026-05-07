0fa9da741355adb2f8ade28947a91a91fb08b3f0
Jireh James <jirehjamesc@gmail.com>
2026-05-08 06:40:44 +0800

    nag add og basic structure

---
+++ landing_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GranGate Landing Page</title>
+    <link rel="stylesheet" href="landing_style.css">
+</head>
+<body>
+    <div class="main-wrapper">
+        </div>
+</body>
+</html>
