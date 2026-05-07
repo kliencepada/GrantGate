da02e82e01c613130c829121d9c294d5e507dc1d
Jireh James <jirehjamesc@gmail.com>
2026-05-07 22:41:12 +0800

    nag implement og php backend logic for user registration

--- register_logic.php
+++ register_logic.php
@@ -1,2 +1,18 @@
 <?php
-include 'db_conn.php';.
+include 'db_conn.php';
+
+if (isset($_POST['register_btn'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $school = mysqli_real_escape_string($conn, $_POST['school']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = $_POST['password']; 
+
+    $query = "INSERT INTO users (name, school, email, password) VALUES ('$name', '$school', '$email', '$password')";
+    
+    if (mysqli_query($conn, $query)) {
+        echo "<script>alert('Registration Successful!'); window.location.href='register.php';</script>";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
+?>
