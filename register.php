798ffb8bb21081c9a909bb6feb7d4b379c7fd56f
Jireh James <jirehjamesc@gmail.com>
2026-05-07 22:30:32 +0800

    nag add og registration form structure

--- register.php
+++ register.php
@@ -1,3 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+<div class="card">
+    <form action="register_logic.php" method="POST">
+        <h2>Sign up</h2>
+        <input type="text" name="name" placeholder="Full Name" required>
+        <input type="text" name="school" placeholder="School" required>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit" name="register_btn">Sign up</button>
+    </form>
+</div>
