b6f79dceb44f5847233833e81ab915df99df1a00
grislicepada <graceliemaecepada3@gmail.com>
2026-05-08 20:21:31 +0800

    nag update sa db_conn para connection sa database

--- db_conn.php
+++ db_conn.php
@@ -1,5 +1,10 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "grantgate_db");
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "grantgate_db";
+
+$con = mysqli_connect($host, $username, $password, $database);
 
 if(!$con) {
     die("Connection Failed: " . mysqli_connect_error());
